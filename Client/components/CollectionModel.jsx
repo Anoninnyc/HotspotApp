7ae43abe985dee39cef707cb3e827c3174677a4c
Krishan Arya <krishanmarya1@gmail.com>
2016-10-20 15:08:36 +1100

    Update_parse

--- Client/components/CollectionModel.jsx
+++ Client/components/CollectionModel.jsx
@@ -5,17 +5,16 @@ function fade (el) {
  $(el).fadeOut(1000);
 }
 
- function parse(res) {
-  let apos = res.indexOf("'");
-  if (apos > -1) {
+function parseAgain(res) {
+  let ast = res.indexOf("*");
+  if (ast > -1) {
     let split = res.split("");
-    split.splice(apos, 1, "*");
+    split.splice(ast, 1, "'");
     return split.join('');
   } else {
     return res;
   }
 }
-
 
 console.log("THIS IS THE PARSE FUNCTION", parse);
 
@@ -29,7 +28,7 @@ var CollectionModel = ({item}) => {
     <img className='card-img-top' src={item.yelpData.image||"http://bit.ly/2e99Pwd"} />
    
     <div className='card-block'>
-      <h4  className='card-title'><a href={url} target="_blank">{parse(item.name)}</a></h4>
+      <h4  className='card-title'><a href={url} target="_blank">{parseAgain(item.name)}</a></h4>
     </div>
     <ul className='list-group list-group-flush'>
       <li className='list-group-item'>Address: <p style={{fontSize:'12px'}}>{address}</p></li>
