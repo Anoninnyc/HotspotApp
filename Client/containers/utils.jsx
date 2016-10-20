61cc5b18935101075b3ecebc9d3922ef06644710
Krishan Arya <krishanmarya1@gmail.com>
2016-10-20 15:00:47 +1100

    Update_utils

--- Client/containers/utils.jsx
+++ Client/containers/utils.jsx
@@ -1,32 +1,9 @@
  function fade (el) {
  $(el).fadeIn(1000);
  $(el).fadeOut(1000);
-}
+ }
 
- function parse(res) {
-  let apos = res.indexOf("'");
-  if (apos > -1) {
-    let split = res.split("");
-    split.splice(apos, 1, "*");
-    return split.join('');
-  } else {
-    return res;
-  }
-}
 
-function parseAgain(res) {
-  let ast = res.indexOf("*");
-  if (ast > -1) {
-    let split = res.split("");
-    split.splice(ast, 1, "'");
-    return split.join('');
-  } else {
-    return res;
-  }
-}
-
-export default {
-  fade,
-  parse,
-  parseAgain,
-}
+ export default {
+   fade,
+ };
